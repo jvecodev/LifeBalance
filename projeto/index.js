570bf6b089ae26d6cefe7186bb653ed8e40ab2d6
jvecodev <jvitor.oliveira1803@gmail.com>
2025-01-14 21:12:24 -0300

    Update index.js

--- projeto/index.js
+++ projeto/index.js
@@ -29,19 +29,19 @@ const connection = await verificarConexao();
 
 
 function autenticarToken(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ error: 'Token não fornecido ou malformado' });
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'Token não fornecido' });
     }
 
-    const token = authHeader.split(' ')[1];
     try {
+ 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
         console.error('Erro ao verificar token:', error);
-        return res.status(403).json({ error: 'Token inválido' });
+        res.status(403).json({ error: 'Token inválido' });
     }
 }
 
